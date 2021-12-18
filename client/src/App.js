a4a1cbf15224676935a6f13b071c4ca2935b1875
eric <howcrazys@gmail.com>
2021-12-18 08:01:44 +0800

    feat: nav register

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import HomeComponent from './components/home-component'
 import NavComponent from './components/nav-component'
+import RegisterComponent from './components/register-component';
 function App(){
   return (
     <div>
       <NavComponent />
       <Routes>
         <Route path="/" element={<HomeComponent />} />
+      </Routes>
+      <Routes>
+        <Route path="/register" element={<RegisterComponent />} />
       </Routes>
     </div>
   )
